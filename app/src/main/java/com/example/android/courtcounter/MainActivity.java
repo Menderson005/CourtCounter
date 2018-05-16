f19929fd055f20d85cbab7fa34ca83a08591a344
Mendy <alm121@hotmail.com>
2018-05-16 06:42:24 -0400

    Changed layout, added Oops button, rotation issue.

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -11,9 +11,32 @@ public class MainActivity extends AppCompatActivity {
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);    }
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        // Always call the superclass so it can save the view hierarchy state per Android Dev
+        super.onSaveInstanceState(outState);
+
+        //Save current info
+        outState.putInt("saved_ScoreA", scoreTeamA);
+        outState.putInt("saved_ScoreB", scoreTeamB);
+
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        // Always call the superclass so it can save the view hierarchy state per Android Dev
+        super.onRestoreInstanceState(savedInstanceState);
+
+        //Restore the scores
+        scoreTeamA = savedInstanceState.getInt("saved_ScoreA");
+        scoreTeamB = savedInstanceState.getInt("saved_ScoreB");
+
+    }
 
     /**
      * Add 3 for Team A
@@ -22,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
         scoreTeamA = scoreTeamA + 3;
         displayForTeamA(scoreTeamA);
     }
+
     /**
      * Add 3 for Team B
      */
@@ -29,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
         scoreTeamB = scoreTeamB + 3;
         displayForTeamB(scoreTeamB);
     }
+
     /**
      * Add 2 for Team A
      */
@@ -36,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
         scoreTeamA = scoreTeamA + 2;
         displayForTeamA(scoreTeamA);
     }
+
     /**
      * Add 2 for Team B
      */
@@ -43,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
         scoreTeamB = scoreTeamB + 2;
         displayForTeamB(scoreTeamB);
     }
+
     /**
      * Add 1 for Team A
      */
@@ -50,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
         scoreTeamA = scoreTeamA + 1;
         displayForTeamA(scoreTeamA);
     }
+
     /**
      * Add 1 for Team B
      */
@@ -58,25 +86,50 @@ public class MainActivity extends AppCompatActivity {
         displayForTeamB(scoreTeamB);
     }
 
+    /**
+     * Reset score to zero when game is over
+     */
     public void reset(View v) {
         scoreTeamA = 0;
-        scoreTeamB = 0;
         displayForTeamA(scoreTeamA);
+        scoreTeamB = 0;
         displayForTeamB(scoreTeamB);
     }
+
+    /**
+     * Subtract 1 for Team A
+     */
+    public void minusOneForTeamA(View v) {
+        scoreTeamA = scoreTeamA - 1;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Subtract 1 for Team B
+     */
+    public void minusOneForTeamB(View v) {
+        scoreTeamB = scoreTeamB - 1;
+        displayForTeamB(scoreTeamB);
+    }
+
+
     /**
      * Displays the given score for Team A.
      */
     public void displayForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        TextView scoreView;
+        scoreView = findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
+
     /**
      * Displays the given score for Team B.
      */
     public void displayForTeamB(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        TextView scoreView;
+        scoreView = findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
+
 
 }
